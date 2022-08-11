61c4a0a796e4a758d7641ce38ab16492a104f7b7
Nian-Tang <110972178+Nian-Tang@users.noreply.github.com>
2022-08-11 09:40:47 +0800

    Update Service.java

--- Service.java
+++ Service.java
@@ -3,8 +3,7 @@ package .service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import .entity.ActivityArea;
-//8.11
-
+//8.11 9.40
  /**
  * ;(activity_area)表服务接口
  * @author : http://www.chiner.pro
